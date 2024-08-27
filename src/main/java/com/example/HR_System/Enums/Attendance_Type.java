26020270daf2e5fbcdd23f8517fbd02cc825b12e
Musab Masalmah <musabsoos10@gmail.com>
2024-08-27 15:19:07 +0300

    enum feature

---
+++ src/main/java/com/example/HR_System/Enums/Attendance_Type.java
@@ -0,0 +1,6 @@
+package com.example.HR_System.Enums;
+
+public enum Attendance_Type {
+    Online,
+    Onsite
+}
--- src/main/java/com/example/HR_System/Models/Attendance.java
+++ src/main/java/com/example/HR_System/Models/Attendance.java
@@ -1,4 +1,12 @@
 package com.example.HR_System.Models;
 
+import java.time.LocalDate;
+
 public class Attendance {
+    private Long id;
+    private Employee employee;
+    private LocalDate date;
+    private LocalDate start_time;
+    private LocalDate end_time;
+
 }
