d2edc6ddc6e0a9a2c2519fc26ff65c21bce4e29a
Mohammadalawia <109400462+Mohammadalawia@users.noreply.github.com>
2024-08-27 15:38:25 +0300

    employee model

---
+++ src/main/java/com/example/HR_System/Enums/Roles.java
@@ -0,0 +1,5 @@
+package com.example.HR_System.Enums;
+
+public enum Roles {
+    User, Hr, Admin
+}
--- src/main/java/com/example/HR_System/Models/Employee.java
+++ src/main/java/com/example/HR_System/Models/Employee.java
@@ -1,4 +1,28 @@
 package com.example.HR_System.Models;
 
+
+import com.example.HR_System.Enums.Roles;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "employee_sequence"
+    )
+    private Long id;
+
+    private String name;
+    private String email;
+    private Byte picture;
+    private Double credit;
+    private String position;
+    private String phone_number;
+    private String address;
+    private Integer max_paid_leave;
+    private Roles role;
 }
